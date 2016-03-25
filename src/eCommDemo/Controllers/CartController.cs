6c56e44569a4366acafeff4e87a8835f19f76780
unknown <walter.gameiro@BAS0682.buildasign.local>
2016-03-25 16:53:59 -0500

    Cart page

--- src/eCommDemo/Controllers/CartController.cs
+++ src/eCommDemo/Controllers/CartController.cs
@@ -12,11 +12,48 @@ namespace eCommDemo.Controllers
         public ActionResult Index()
         {
             var model = new CartModel();
+            model.Items = new List<CartItem>
+            {
+                new CartItem {SKU = "CNV-123", Description = "Arizona 16x20", Price = 100m, Discount = 0, Quantity = 1, Image = "/Images/Listings/arizona.jpg"}
+            };
             return View(model);
         }
     }
 
     public class CartModel
     {
+        public CartModel()
+        {
+            Items = new List<CartItem>();
+            CartId = Guid.NewGuid();
+        }
+
+        public Guid CartId { get; set; }
+        public List<CartItem> Items { get; set; }
+
+        public decimal Subtotal
+        {
+            get { return Items.Sum(ci => ci.Price); }
+        }
+
+        public decimal Discounts
+        {
+            get { return Items.Sum(ci => ci.Discount); }
+        }
+
+        public decimal Total
+        {
+            get { return Subtotal - Discounts; }
+        }
+    }
+
+    public class CartItem
+    {
+        public string SKU { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
     }
 }
