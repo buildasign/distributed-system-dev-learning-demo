68c47a1e2c341aabe77c0b479e93fbfb13f77502
unknown <walter.gameiro@BAS0682.buildasign.local>
2016-03-25 13:04:26 -0500

    Setting up basic ui framework - temp check in for Tom

---
+++ src/eCommDemo/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var model = new CategoriesModel();
+            return View(model);
+        }
+    }
+
+    public class CategoriesModel
+    {
+        public CategoriesModel()
+        {
+            Categories = new List<Category>(new[] { new Category { Id = 1, Name = "Single Canvas", Image = "/Images/Categories/Canvas.png" }, new Category { Id = 2, Name = "Wall Display", Image = "/Images/Categories/WallDisplay.png" }});
+        }
+
+        public List<Category> Categories { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ src/eCommDemo/Controllers/ListingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommDemo.Controllers
+{
+    public class ListingsController : Controller
+    {
+        // GET: Listings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
