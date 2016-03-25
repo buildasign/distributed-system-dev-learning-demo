e105955a6ad3d526b28a2711411f1eb9c055e13b
unknown <walter.gameiro@BAS0682.buildasign.local>
2016-03-25 14:28:03 -0500

    Basic flow complete

---
+++ src/eCommDemo/Controllers/CheckoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommDemo.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            var model = new CheckoutModel();
+            return View(model);
+        }
+    }
+
+    public class CheckoutModel
+    {
+    }
+}
